fc3198d8f23ec9308b2d8296805c9486742cfaa3
Leandro0101 <leandrovieira3306@gmail.com>
2021-01-02 15:05:30 -0300

    using CommandLineRunner

---
+++ src/main/java/com/devstudent/workshopmongo/config/Instantiation.java
@@ -0,0 +1,27 @@
+package com.devstudent.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.devstudent.workshopmongo.domain.User;
+import com.devstudent.workshopmongo.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex green", "alex@gmail.com");
+		User bob = new User(null, "Bob grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
