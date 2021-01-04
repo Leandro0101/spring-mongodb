95937022a2f74b79e2e9d4f4e06b5fa6d6dd4c24
Leandro0101 <leandrovieira3306@gmail.com>
2021-01-03 22:22:00 -0300

    insert user

--- src/main/java/com/devstudent/workshopmongo/services/UserService.java
+++ src/main/java/com/devstudent/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devstudent.workshopmongo.domain.User;
+import com.devstudent.workshopmongo.dto.UserDTO;
 import com.devstudent.workshopmongo.repositories.UserRepository;
 import com.devstudent.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 		User user = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(id));
 		
 		return user;
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
